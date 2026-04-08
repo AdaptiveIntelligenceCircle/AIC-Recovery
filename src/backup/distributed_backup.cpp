0899323f87bb8488a5af8ccca474d03fb7972bcc
ndtribk <ndtribk@gmail.com>
2026-04-08 17:27:32 +0700

    update

--- src/backup/distributed_backup.cpp
+++ src/backup/distributed_backup.cpp
@@ -1,6 +1,22 @@
 #include "distributed_backup.h"
+#include <iostream>
 
 namespace aic :: recovery 
 {
-    
+    bool DistributedBackup :: createDistributeBackup(const string &nodeId)
+    {
+        cout << "AIC-Recovery : Creating distributed backup for node" << nodeId << endl; 
+        return true; 
+    }
+
+    bool DistributedBackup :: restoreFromBackup(const string &nodeId)
+    {
+        cout << "AIC-Recovery: Restoring Node" << nodeId << "from distributed backup...\\n"; 
+        return true; 
+    }
+
+    vector<string> DistributedBackup :: getHealthyNodes() const 
+    {
+        return {"node-001" , "node-002", "node-157"};
+    }
 }
